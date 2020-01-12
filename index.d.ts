4eb42ba5eb30804c9e2c27b93273170436b26d80
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-01-12 18:59:37 +0900

    chore: add tsc check to ci

--- index.d.ts
+++ index.d.ts
@@ -7,46 +7,48 @@ import {
   NativeSyntheticEvent,
   NativeSegmentedControlIOSChangeEvent,
   NativeMethodsMixin,
-  Constructor
-} from 'react-native'
+  Constructor,
+} from 'react-native';
 
 export interface SegmentedControlIOSProps extends ViewProps {
-    /**
-     * If false the user won't be able to interact with the control. Default value is true.
-     */
-    enabled?: boolean;
+  /**
+   * If false the user won't be able to interact with the control. Default value is true.
+   */
+  enabled?: boolean;
 
-    /**
-     * If true, then selecting a segment won't persist visually.
-     * The onValueChange callback will still work as expected.
-     */
-    momentary?: boolean;
+  /**
+   * If true, then selecting a segment won't persist visually.
+   * The onValueChange callback will still work as expected.
+   */
+  momentary?: boolean;
 
-    /**
-     * Callback that is called when the user taps a segment;
-     * passes the event as an argument
-     */
-    onChange?: (event: NativeSyntheticEvent<NativeSegmentedControlIOSChangeEvent>) => void;
+  /**
+   * Callback that is called when the user taps a segment;
+   * passes the event as an argument
+   */
+  onChange?: (
+    event: NativeSyntheticEvent<NativeSegmentedControlIOSChangeEvent>,
+  ) => void;
 
-    /**
-     * Callback that is called when the user taps a segment; passes the segment's value as an argument
-     */
-    onValueChange?: (value: string) => void;
+  /**
+   * Callback that is called when the user taps a segment; passes the segment's value as an argument
+   */
+  onValueChange?: (value: string) => void;
 
-    /**
-     * The index in props.values of the segment to be (pre)selected.
-     */
-    selectedIndex?: number;
+  /**
+   * The index in props.values of the segment to be (pre)selected.
+   */
+  selectedIndex?: number;
 
-    /**
-     * Accent color of the control.
-     */
-    tintColor?: string;
+  /**
+   * Accent color of the control.
+   */
+  tintColor?: string;
 
-    /**
-     * The labels for the control's segment buttons, in order.
-     */
-    values?: string[];
+  /**
+   * The labels for the control's segment buttons, in order.
+   */
+  values?: string[];
 }
 
 /**
@@ -69,6 +71,9 @@ export interface SegmentedControlIOSProps extends ViewProps {
  * />
  * ````
  */
-declare class SegmentedControlIOSComponent extends React.Component<SegmentedControlIOSProps> {}
-declare const SegmentedControlIOSBase: Constructor<NativeMethodsMixin> & typeof SegmentedControlIOSComponent;
+declare class SegmentedControlIOSComponent extends React.Component<
+  SegmentedControlIOSProps
+> {}
+declare const SegmentedControlIOSBase: Constructor<NativeMethodsMixin> &
+  typeof SegmentedControlIOSComponent;
 export default class SegmentedControlIOS extends SegmentedControlIOSBase {}
