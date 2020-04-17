cdce6a590a18ba04d36dc75e57b2b22cbe1a7223
Jesse Katsumata <niconico.clarinet@gmail.com>
2020-04-17 17:41:08 +0900

    feat: add SegmentedControl for android (#65)
    * 1.3.0
    
    * feat: add android component

--- js/RNCSegmentedControlNativeComponent.js
+++ js/RNCSegmentedControlNativeComponent.js
@@ -5,83 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  *
  * @flow
- * @format
  */
 'use strict';
 
 import {requireNativeComponent} from 'react-native';
-
-import type {ViewProps} from 'react-native/Libraries/Components/View/ViewPropTypes';
-import type {SyntheticEvent} from 'react-native/Libraries/Types/CoreEventTypes';
 import type {NativeComponent} from 'react-native/Libraries/Renderer/shims/ReactNative';
-import type {ColorValue} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
+import type {SegmentedControlProps} from './types';
 
-type Event = SyntheticEvent<
-  $ReadOnly<{|
-    value: number,
-    selectedSegmentIndex: number,
-  |}>,
->;
-
-export type SegmentedControlIOSProps = $ReadOnly<{|
-  ...ViewProps,
-  /**
-   * The labels for the control's segment buttons, in order.
-   */
-  values?: $ReadOnlyArray<string>,
-  /**
-   * The index in `props.values` of the segment to be (pre)selected.
-   */
-  selectedIndex?: ?number,
-  /**
-   * Callback that is called when the user taps a segment;
-   * passes the segment's value as an argument
-   */
-  onValueChange?: ?(value: number) => mixed,
-  /**
-   * Callback that is called when the user taps a segment;
-   * passes the event as an argument
-   */
-  onChange?: ?(event: Event) => mixed,
-  /**
-   * If false the user won't be able to interact with the control.
-   * Default value is true.
-   */
-  enabled?: boolean,
-  /**
-   * Accent color of the control.
-   */
-  tintColor?: ?ColorValue,
-  /**
-   * Text color of the control.
-   * NOTE: this prop will only work for iOS >= 13
-   */
-  textColor?: ?ColorValue,
-  /**
-   * Text color of the control when selected.
-   * NOTE: this prop will only work for iOS >= 13
-   */
-  activeTextColor?: ?ColorValue,
-  /**
-   * Background color of the control.
-   * NOTE: this prop will only work for iOS >= 13
-   */
-  backgroundColor?: ?ColorValue,
-  /**
-   * If true, then selecting a segment won't persist visually.
-   * The `onValueChange` callback will still work as expected.
-   */
-  momentary?: ?boolean,
-  /**
-   * (iOS 13 only)
-   * Overrides the control's appearance irrespective of the OS theme
-   */
-  appearance?: 'dark' | 'light',
-|}>;
-
-type NativeSegmentedControlIOS = Class<
-  NativeComponent<SegmentedControlIOSProps>,
->;
+type NativeSegmentedControlIOS = Class<NativeComponent<SegmentedControlProps>>;
 
 module.exports = ((requireNativeComponent(
   'RNCSegmentedControl',
---
+++ js/SegmentedControlTab.js
@@ -0,0 +1,77 @@
+/**
+ * @flow
+ */
+
+'use strict';
+
+import * as React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+
+type Props = $ReadOnly<{|
+  value: string,
+  tintColor?: string,
+  textColor?: string,
+  activeTextColor?: string,
+  onSelect: () => void,
+  selected: Boolean,
+  enabled: Boolean,
+|}>;
+
+export const SegmentedControlTab = ({
+  onSelect,
+  value,
+  enabled,
+  selected,
+  tintColor,
+  textColor,
+  activeTextColor,
+}: Props) => {
+  const getColor = () => {
+    if (selected && activeTextColor) {
+      return activeTextColor;
+    }
+    if (textColor) {
+      return textColor;
+    }
+    if (tintColor) {
+      return tintColor;
+    }
+    return 'black';
+  };
+  const color = getColor();
+
+  const getBackgroundColor = () => {
+    if (selected && tintColor) {
+      return tintColor;
+    }
+    return 'white';
+  };
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      disabled={!enabled}
+      onPress={onSelect}>
+      <View
+        style={[
+          styles.default,
+          selected && {backgroundColor: getBackgroundColor()},
+        ]}>
+        <Text style={[{color}, selected && styles.activeText]}>{value}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {flex: 1, borderRadius: 5},
+  default: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 2,
+    borderRadius: 5,
+  },
+  activeText: {
+    fontWeight: '700',
+  },
+});
