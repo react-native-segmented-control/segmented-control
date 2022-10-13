a62839f524a3be9a3280f93f4fccbd31ec4d9217
Yajana N Rao <yajananrao@gmail.com>
2022-10-13 22:01:38 +0530

    Updated example project

--- example/App.js
+++ example/App.js
@@ -2,112 +2,183 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
- * @format
- * @flow strict-local
+ * @flow
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
+import React, {useEffect, useState} from 'react';
+import {ScrollView, StyleSheet, Text, View, useColorScheme} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
+  const colorScheme = useColorScheme();
+  const [textColor, setTextColor] = useState('#000');
+  const [value, setValue] = useState('Unselected');
+  const [selectedIndex, setSelectedIndex] = useState(undefined);
+
+  useEffect(() => {
+    setTextColor(colorScheme === 'dark' ? '#FFF' : '#000');
+  }, [colorScheme]);
+
+  const _onChange = (event) => {
+    setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
+  };
+
+  const _onValueChange = (val) => {
+    setValue(val);
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <ScrollView
+      contentContainerStyle={[
+        styles.container,
+        {backgroundColor: colorScheme === 'dark' ? '#000' : '#FFF'},
+      ]}>
+      <View style={styles.segmentContainer}>
+        <Text style={[styles.text, {color: textColor}]}>
+          Segmented controls can have values and images
+        </Text>
+        <SegmentedControl
+          values={['One', 'Two', require('./assets/images/user.png')]}
+        />
+      </View>
+      <View style={styles.segmentSection}>
+        <SegmentedControl
+          values={[
+            'One',
+            'Two',
+            require('../assets/images/user.png'),
+            'Three',
+            'Four',
+            'Five',
+          ]}
+        />
+      </View>
+      <View style={styles.segmentSection}>
+        <Text style={[styles.text, {color: textColor}]}>
+          Segmented controls can have pre-selected values
+        </Text>
+        <SegmentedControl values={['One', 'Two']} selectedIndex={0} />
+      </View>
+      <View style={styles.segmentSection}>
+        <Text style={[styles.text, {color: textColor}]}>
+          Segmented controls can be momentary
+        </Text>
+        <SegmentedControl values={['One', 'Two']} momentary={true} />
+      </View>
+      <View style={styles.segmentSection}>
+        <Text style={[styles.text, {color: textColor}]}>
+          Segmented controls can be disabled
+        </Text>
+        <SegmentedControl
+          enabled={false}
+          values={['One', 'Two']}
+          selectedIndex={1}
+        />
+      </View>
+      <View style={styles.segmentContainer}>
+        <Text style={[styles.text, {color: textColor}]}>
+          Custom colors can be provided
+        </Text>
+        <SegmentedControl
+          tintColor="#ff0000"
+          values={['One', 'Two', 'Three', 'Four']}
+          selectedIndex={0}
+          backgroundColor="#0000ff"
+        />
+      </View>
+      <View style={styles.segmentContainer}>
+        <SegmentedControl
+          tintColor="#00ff00"
+          values={['One', 'Two', 'Three']}
+          selectedIndex={1}
+        />
+      </View>
+      <View style={styles.segmentSection}>
+        <SegmentedControl
+          fontStyle={{color: '#ff00ff'}}
+          activeFontStyle={{color: 'blue'}}
+          values={['One', 'Two']}
+          selectedIndex={1}
+        />
+      </View>
+      <View style={styles.segmentContainer}>
+        <Text style={[styles.text, {color: textColor}]}>
+          Segmented controls can have defined fontSize
+        </Text>
+        <View style={styles.segmentContainer}>
+          <SegmentedControl
+            values={['One', 'Two']}
+            style={{height: 80}}
+            fontStyle={styles.fontStyle}
+          />
+        </View>
+        <SegmentedControl
+          values={['One', 'Two']}
+          tintColor="red"
+          style={{height: 80}}
+          fontStyle={{
+            fontFamily: 'Optima',
+            fontSize: 32,
+          }}
+          activeFontStyle={styles.activeFontStyle}
+        />
+      </View>
+      <View>
+        <Text style={[styles.text, {color: textColor}]}>
+          Custom colors can be provided
+        </Text>
+        <View style={styles.segmentContainer}>
+          <SegmentedControl
+            values={['One', 'Two', 'Three']}
+            selectedIndex={selectedIndex}
+            onChange={_onChange}
+            onValueChange={_onValueChange}
+          />
+        </View>
+        <Text style={[styles.text, {color: textColor}]}>
+          Value: {value} Index: {selectedIndex}
+        </Text>
+      </View>
+
+      <View>
+        <Text style={[styles.text, {color: textColor}]}>
+          Appearance value can be provided
+        </Text>
+        <View style={styles.segmentContainer}>
+          <SegmentedControl
+            appearance="light"
+            values={['One', 'Two', 'Three']}
+            selectedIndex={1}
+          />
+        </View>
+        <View style={styles.segmentContainer}>
+          <SegmentedControl
+            appearance="dark"
+            values={['One', 'Two', 'Three']}
+            selectedIndex={2}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  text: {
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '500',
+    margin: 10,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  segmentContainer: {
+    marginBottom: 10,
   },
-  body: {
-    backgroundColor: Colors.white,
+  segmentSection: {
+    marginBottom: 25,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    paddingTop: 80,
   },
 });
 
