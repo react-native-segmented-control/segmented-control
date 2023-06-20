e6453e98f29b70828a72f0cd9db96732bbbca263
Vojtech Novak <vonovak@gmail.com>
2023-06-20 15:47:28 +0200

    fix: content not visible on js version

--- js/SegmentedControl.js
+++ js/SegmentedControl.js
@@ -92,6 +92,19 @@ const SegmentedControl = ({
           selectedIndex={selectedIndex}
         />
       )}
+      {selectedIndex != null && segmentWidth ? (
+        <Animated.View
+          style={[
+            styles.slider,
+            {
+              transform: [{translateX: animation}],
+              width: segmentWidth - 4,
+              backgroundColor:
+                tintColor || (colorScheme === 'dark' ? '#636366' : 'white'),
+            },
+          ]}
+        />
+      ) : null}
       <View style={styles.segmentsContainer}>
         {values &&
           values.map((value, index) => {
@@ -113,19 +126,6 @@ const SegmentedControl = ({
             );
           })}
       </View>
-      {selectedIndex != null && segmentWidth ? (
-        <Animated.View
-          style={[
-            styles.slider,
-            {
-              transform: [{translateX: animation}],
-              width: segmentWidth - 4,
-              backgroundColor:
-                tintColor || (colorScheme === 'dark' ? '#636366' : 'white'),
-            },
-          ]}
-        />
-      ) : null}
     </View>
   );
 };
