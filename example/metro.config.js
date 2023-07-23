0d9d0bfdc6c3799da7be9a0d226c824089232a3b
Jesse Katsumata <jesse.katsumata@gmail.com>
2023-07-23 10:31:56 +0900

    fix: lint

--- example/metro.config.js
+++ example/metro.config.js
@@ -16,7 +16,7 @@ const exclusionList = (() => {
   }
 })();
 
-const { makeMetroConfig } = require('@rnx-kit/metro-config');
+const {makeMetroConfig} = require('@rnx-kit/metro-config');
 const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
 const symlinkResolver = MetroSymlinksResolver();
